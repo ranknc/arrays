09b6fa721ce77655264abffde3c01fdc2e14da19
Nick Rank <ranknc@s.dcsdk12.org>
2015-09-21 09:22:47 -0600

    idek

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,11 @@
 module Arrays
-	
+
+	def middles(array1, array2)	
+		[array1[1], array2[1]]
+
+	end
+
+
 	
 	
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -11,4 +11,88 @@ class ArraysTest < MiniTest::Test
    	end.new
 	end
 
+	def test_middles
+		assert_equal [5, 2], @m.middles([1, 2, 3], [4, 5, 6])
+		# assert_equal 4, @m.middles()
+		# assert_equal , @m.middles()
+		# assert_equal , @m.middles()
+		
+	end
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+	
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+	
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+	
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+	
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+	
+
+	# def test_
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+	# 	assert_equal , @m.()
+		
+	# end
+
 end
