1c1001de39b97d88e7dc999cb09ae78e8f8b197f
Nick Rank <ranknc@s.dcsdk12.org>
2015-09-21 21:07:24 -0600

    Struggling

--- arrays.rb
+++ arrays.rb
@@ -5,7 +5,54 @@ module Arrays
 
 	end
 
-#cant push
+	def max(array)
+		n = 0
+		array.each do |element|
+			if element > n 
+				n = element
+			end
+		end
+		n
+	end	
+
+	def lucky13?(array)
+		array.each do |element|
+			if element == 1 || element == 3
+				return false
+			end
+		end
+	end
+
+	def repeat_separator(word, separator, count)
+		str = ""
+		(1..count).each do |n|
+			str += word + separator
+		end
+		str += word 
+	end
+
+	def same_ends?(array, n)
+		str1 = ""
+		str2 = ""
+		(0...range).each do |n|
+			str1 += array[n].to_s
+		end
+		(array.length - range...array.length).each do |n|
+			str2 += array[n].to_s
+		end
+		str1 == str2
+	end
+	
+	def plus_it(word, token)
+		for i in 0..(word.length-token.length)
+			check = word(i..(i+token.length))
+			if check == token
+				i += token.length
+			else
+				word[i] = '+'
+			end
+		end
+	end
 	
 	
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -13,86 +13,37 @@ class ArraysTest < MiniTest::Test
 
 	def test_middles
 		assert_equal [5, 2], @m.middles([1, 2, 3], [4, 5, 6])
-		# assert_equal 4, @m.middles()
-		# assert_equal , @m.middles()
-		# assert_equal , @m.middles()
 		
 	end
 
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
+	def test_max
+		assert_equal 9, @m.max([1, 3, 5, 7, 9])
+		assert_equal 10, @m.max([2, 4, 6, 8, 10])
+	end
 
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
-
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
+	def test_lucky13?
+		assert_equal true, @m.lucky13?([5, 6, 7, 8])
+		assert_equal true, @m.lucky13?([2, 4, 6, 8])
+		assert_equal false, @m.lucky13?([1, 5, 25,])
+		assert_equal false, @m.lucky13?([3, 6, 9])
+	end
 	
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
+	def test_repeat_separator
+		assert_equal "HiComputerHi", @m.repeat_separator("Hi", "Computer", 2)
+		assert_equal "LOL", @m.repeat_separator("L", "O", 2)
+		assert_equal "Wow", @m.repeat_separator("Wow", "Man", 1)
+	end
 
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
-	
+	def test_same_ends?
+		assert_equal true, @m.same_ends?([10, 20, 30, 40, 10, 20], 2)
+		assert_equal true, @m.same_ends?([5, 5, 6], 0)
+		assert_equal false, @m.same_ends?([1, 2, 3, 4, 5, 6], 3)
+	end
+	def test_plus_it
+		assert_equal "+LOL+" , @m.plus_it("1LOL2", "LOL")
+		assert_equal "++++" , @m.plus_it("Nick", 'John')
+		assert_equal "rank++" , @m.plus_it("Ranknc", 'Rank')
+	end
 
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
-	
-
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
-	
-
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
-	
-
-	# def test_
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-	# 	assert_equal , @m.()
-		
-	# end
 
 end
